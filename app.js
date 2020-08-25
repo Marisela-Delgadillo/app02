0b2260317f7c7398ce5b24e7f73b01ab276032d4
unknown <marisela.delgadillo.cbtis37@gmail.com>
2020-08-25 16:14:04 -0700

    pokemon

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ const pokemon = {
     sprite: document.getElementById('pokemon-image'),
     name: document.getElementById('pokemon-name'),
     types: document.getElementById('pokemon-types'),
-    abilities: document.getElementById('pokemon-abilities')
+    abilities: document.getElementById('pokemon-abilities'),
    // egg: document.getElementById('pokemon-egg'),
     //type: document.getElementById('pokemon-type'),
     //ability1: document.getElementById('pokemon-ability1')
 
+    sprite1: document.getElementById('pokemon1'),
+    sprite2: document.getElementById('pokemon2'),
+    sprite3: document.getElementById('pokemon3'),
+    sprite4: document.getElementById('pokemon4'),
+    sprite5: document.getElementById('pokemon5'),
+    sprite6: document.getElementById('pokemon6'),
+    sprite7: document.getElementById('pokemon7'),
+    sprite8: document.getElementById('pokemon8'),
+    sprite9: document.getElementById('pokemon9'),
+    sprite10: document.getElementById('pokemon10')
+
 
 }
-
-
 
 const GetPokemon = async ()=>{
     const url='https://pokeapi.co/api/v2/pokemon/jigglypuff';
@@ -74,3 +83,104 @@ const GetAbilityInfo= async (url, abilityName)=>{
 }
 
 GetPokemon();
+
+const GetPikachu= async ()=>{
+    const url=' https://pokeapi.co/api/v2/pokemon/pikachu';
+    const data= await fetch(url);
+    const dataJson = await data.json();
+    const{sprites}= dataJson;
+    pokemon.sprite1.src = await sprites.front_default;
+
+}
+GetPikachu();
+
+const GetCharmander= async ()=>{
+    const url=' https://pokeapi.co/api/v2/pokemon/charmander';
+    const data= await fetch(url);
+    const dataJson = await data.json();
+    const{sprites}= dataJson;
+    pokemon.sprite2.src = await sprites.front_default;
+
+}
+GetCharmander();
+
+const GetBulbasaur= async ()=>{
+    const url= 'https://pokeapi.co/api/v2/pokemon/bulbasaur';
+    const data= await fetch(url);
+    const dataJson = await data.json();
+    const{sprites}= dataJson;
+    pokemon.sprite3.src = await sprites.front_default;
+
+}
+GetBulbasaur();
+
+const GetSquirtle= async ()=>{
+    const url= ' https://pokeapi.co/api/v2/pokemon/squirtle';
+    const data= await fetch(url);
+    const dataJson = await data.json();
+    const{sprites}= dataJson;
+    pokemon.sprite4.src = await sprites.front_default;
+
+}
+GetSquirtle();
+
+const GetPidgeotto= async ()=>{
+    const url= 'https://pokeapi.co/api/v2/pokemon/pidgeotto';
+    const data= await fetch(url);
+    const dataJson = await data.json();
+    const{sprites}= dataJson;
+    pokemon.sprite5.src = await sprites.front_default;
+
+}
+GetPidgeotto();
+
+const GetEkans= async ()=>{
+    const url= 'https://pokeapi.co/api/v2/pokemon/ekans';
+    const data= await fetch(url);
+    const dataJson = await data.json();
+    const{sprites}= dataJson;
+    pokemon.sprite6.src = await sprites.front_default;
+
+}
+GetEkans();
+
+const GetRaichu= async ()=>{
+    const url= ' https://pokeapi.co/api/v2/pokemon/raichu';
+    const data= await fetch(url);
+    const dataJson = await data.json();
+    const{sprites}= dataJson;
+    pokemon.sprite7.src = await sprites.front_default;
+
+}
+GetRaichu();
+
+const GetNidorina= async ()=>{
+    const url= 'https://pokeapi.co/api/v2/pokemon/nidorina';
+    const data= await fetch(url);
+    const dataJson = await data.json();
+    const{sprites}= dataJson;
+    pokemon.sprite8.src = await sprites.front_default;
+
+}
+GetNidorina();
+
+const GetGolbat= async ()=>{
+    const url= 'https://pokeapi.co/api/v2/pokemon/golbat';
+    const data= await fetch(url);
+    const dataJson = await data.json();
+    const{sprites}= dataJson;
+    pokemon.sprite9.src = await sprites.front_default;
+
+}
+GetGolbat();
+
+const GetMankey= async ()=>{
+    const url= 'https://pokeapi.co/api/v2/pokemon/mankey';
+    const data= await fetch(url);
+    const dataJson = await data.json();
+    const{sprites}= dataJson;
+    pokemon.sprite10.src = await sprites.front_default;
+
+}
+GetMankey();
+
