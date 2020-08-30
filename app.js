26c1f4f5ec73fc644d213aa5ee58beb471da281b
unknown <marisela.delgadillo.cbtis37@gmail.com>
2020-08-30 16:44:25 -0700

    pokemon info

--- app.js
+++ app.js
@@ -1,53 +1,65 @@
-const pokemon = {
-    sprite: document.getElementById('pokemon-image'),
-    name: document.getElementById('pokemon-name'),
-    types: document.getElementById('pokemon-types'),
-    abilities: document.getElementById('pokemon-abilities'),
-   // egg: document.getElementById('pokemon-egg'),
-    //type: document.getElementById('pokemon-type'),
-    //ability1: document.getElementById('pokemon-ability1')
+//     const pokemon = {
+//     sprite: document.getElementById('pokemon-image'),
+//     name: document.getElementById('pokemon-name'),
+//     types: document.getElementById('pokemon-types'),
+//     abilities: document.getElementById('pokemon-abilities'),
+// }
 
-    sprite1: document.getElementById('pokemon1'),
-    sprite2: document.getElementById('pokemon2'),
-    sprite3: document.getElementById('pokemon3'),
-    sprite4: document.getElementById('pokemon4'),
-    sprite5: document.getElementById('pokemon5'),
-    sprite6: document.getElementById('pokemon6'),
-    sprite7: document.getElementById('pokemon7'),
-    sprite8: document.getElementById('pokemon8'),
-    sprite9: document.getElementById('pokemon9'),
-    sprite10: document.getElementById('pokemon10')
+const baseUrl = 'https://pokeapi.co/api/v2';
+const pokemonImages = document.getElementById('pokemon-images');
 
-
-}
-
-const GetPokemon = async ()=>{
-    const url='https://pokeapi.co/api/v2/pokemon/jigglypuff';
-
+const GetPokemon = async url=>{
     const data= await fetch(url);
     const dataJson = await data.json();
 
     const{sprites, name, types, abilities}= dataJson;
 
-    types.forEach(element => { //forEach para recorrer arreglos
-        const {type} = element;
-        //console.log(type.name);
-        pokemon.types.innerHTML += `<li>${type.name}</li>`;
+    // types.forEach(element => { //forEach para recorrer arreglos
+    //      const {type} = element;
+    //      console.log(type.name);
+    //      pokemon.types.innerHTML += `<li>${type.name}</li>`;
 
+    //  });
+
+    //  abilities.forEach(element => {
+    //      const{ability}= element;
+    //      //pokemon.abilities.innerHTML += `<li>${ability.name}</li>`;
+    //      //console.log(ability.url);
+    //      GetAbilityInfo(ability.url, ability.name);
+    // })
+
+    // console.log(await abilities);
+
+    // pokemon.sprite.src = await dataJson.sprites['front_default'];
+    // pokemon.name.innerHTML= await `Name: ${name}`;
+    return await sprites.front_default;
+
+}
+
+
+
+const GetPokemonList = async ()=>{
+    const url = `${baseUrl}/pokemon`;
+    fetch(url).then(data => data.json()).then(json =>{
+        //console.log(json.results);
+        const urlList = json.results.map(element => element.url);
+        //console.log(urlList);
+        const spriteList = urlList.map(pokemonUrl => GetPokemon(pokemonUrl));
+        spriteList.forEach(async sprite => {
+            await sprite;
+            const currentPokemonImg = document.createElement('img');
+            currentPokemonImg.src = await sprite;
+            currentPokemonImg.className = 'pokemonImage';
+            pokemonImages.appendChild(currentPokemonImg);
+
+            currentPokemonImg.onclick = ()=> {
+                sessionStorage.setItem('urlList', JSON.stringify(urlList));
+                sessionStorage.setItem('sprite', currentPokemonImg.src);
+                window.location.href = 'file:///D:/Documentos/programacion-hipermedia/app02/pokemon.htm';
+            }
+
+        });
     });
-
-    abilities.forEach(element => {
-        const{ability}= element;
-        //pokemon.abilities.innerHTML += `<li>${ability.name}</li>`;
-        //console.log(ability.url);
-        GetAbilityInfo(ability.url, ability.name);
-    })
-
-    //console.log(await abilities);
-
-    pokemon.sprite.src = await dataJson.sprites['front_default'];
-    pokemon.name.innerHTML= await `Name: ${name}`;
-
 }
 
 const GetAbilityInfo= async (url, abilityName)=>{
@@ -62,7 +74,7 @@ const GetAbilityInfo= async (url, abilityName)=>{
         const{effect, language} = element;
 
         if(language.name === 'en'){
-            console.log(effect);
+            //console.log(effect);
             effectsList += `<li>${effect}</li>`;
         }
     });
@@ -82,105 +94,6 @@ const GetAbilityInfo= async (url, abilityName)=>{
     //console.log(await dataJson);
 }
 
-GetPokemon();
-
-const GetPikachu= async ()=>{
-    const url=' https://pokeapi.co/api/v2/pokemon/pikachu';
-    const data= await fetch(url);
-    const dataJson = await data.json();
-    const{sprites}= dataJson;
-    pokemon.sprite1.src = await sprites.front_default;
-
-}
-GetPikachu();
-
-const GetCharmander= async ()=>{
-    const url=' https://pokeapi.co/api/v2/pokemon/charmander';
-    const data= await fetch(url);
-    const dataJson = await data.json();
-    const{sprites}= dataJson;
-    pokemon.sprite2.src = await sprites.front_default;
-
-}
-GetCharmander();
-
-const GetBulbasaur= async ()=>{
-    const url= 'https://pokeapi.co/api/v2/pokemon/bulbasaur';
-    const data= await fetch(url);
-    const dataJson = await data.json();
-    const{sprites}= dataJson;
-    pokemon.sprite3.src = await sprites.front_default;
-
-}
-GetBulbasaur();
-
-const GetSquirtle= async ()=>{
-    const url= ' https://pokeapi.co/api/v2/pokemon/squirtle';
-    const data= await fetch(url);
-    const dataJson = await data.json();
-    const{sprites}= dataJson;
-    pokemon.sprite4.src = await sprites.front_default;
-
-}
-GetSquirtle();
-
-const GetPidgeotto= async ()=>{
-    const url= 'https://pokeapi.co/api/v2/pokemon/pidgeotto';
-    const data= await fetch(url);
-    const dataJson = await data.json();
-    const{sprites}= dataJson;
-    pokemon.sprite5.src = await sprites.front_default;
-
-}
-GetPidgeotto();
-
-const GetEkans= async ()=>{
-    const url= 'https://pokeapi.co/api/v2/pokemon/ekans';
-    const data= await fetch(url);
-    const dataJson = await data.json();
-    const{sprites}= dataJson;
-    pokemon.sprite6.src = await sprites.front_default;
-
-}
-GetEkans();
-
-const GetRaichu= async ()=>{
-    const url= ' https://pokeapi.co/api/v2/pokemon/raichu';
-    const data= await fetch(url);
-    const dataJson = await data.json();
-    const{sprites}= dataJson;
-    pokemon.sprite7.src = await sprites.front_default;
-
-}
-GetRaichu();
-
-const GetNidorina= async ()=>{
-    const url= 'https://pokeapi.co/api/v2/pokemon/nidorina';
-    const data= await fetch(url);
-    const dataJson = await data.json();
-    const{sprites}= dataJson;
-    pokemon.sprite8.src = await sprites.front_default;
-
-}
-GetNidorina();
-
-const GetGolbat= async ()=>{
-    const url= 'https://pokeapi.co/api/v2/pokemon/golbat';
-    const data= await fetch(url);
-    const dataJson = await data.json();
-    const{sprites}= dataJson;
-    pokemon.sprite9.src = await sprites.front_default;
-
-}
-GetGolbat();
-
-const GetMankey= async ()=>{
-    const url= 'https://pokeapi.co/api/v2/pokemon/mankey';
-    const data= await fetch(url);
-    const dataJson = await data.json();
-    const{sprites}= dataJson;
-    pokemon.sprite10.src = await sprites.front_default;
-
-}
-GetMankey();
+GetPokemon('https://pokeapi.co/api/v2/pokemon/jigglypuff');
+GetPokemonList();
 
---
+++ pokemon.js
@@ -0,0 +1,88 @@
+const urlList = JSON.parse(sessionStorage.getItem('urlList'));
+const sprite = sessionStorage.getItem('sprite');
+const pokemon = {
+    sprite: document.getElementById('pokemon-image'),
+    name: document.getElementById('pokemon-name'),
+    types: document.getElementById('pokemon-types'),
+    abilities: document.getElementById('pokemon-abilities')
+}
+
+
+
+//console.log(urlList);
+const GetPokemon = () => {
+    const spriteName = sprite.replace('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/', '');
+    const spriteNumber = spriteName.replace('.png', '');
+    console.log(spriteNumber);
+    urlList.forEach(element => {
+        let pokemonNumber = element.replace('https://pokeapi.co/api/v2/pokemon/', '');
+        pokemonNumber = pokemonNumber.replace('/', '');
+        if (spriteNumber == pokemonNumber) {
+            console.log(element);
+            GetPokemonInfo(element);
+
+        }
+    });
+
+}
+
+const GetPokemonInfo = async (url) => {
+    const data = await fetch(url);
+    const dataJson = await data.json();
+    const { sprites, name, types, abilities } = dataJson;
+    types.forEach(element => { //forEach para recorrer arreglos
+          const {type} = element;
+    //      console.log(type.name);
+          pokemon.types.innerHTML += `<li>${type.name}</li>`;
+
+      });
+
+      abilities.forEach(element => {
+          const{ability}= element;
+    //      //pokemon.abilities.innerHTML += `<li>${ability.name}</li>`;
+    //      //console.log(ability.url);
+         GetAbilityInfo(ability.url, ability.name);
+     })
+
+    // console.log(await abilities);
+
+    pokemon.sprite.src = await dataJson.sprites['front_default'];
+    pokemon.name.innerHTML= await `Name: ${name}`;
+    //return await sprites.front_default;
+}
+
+
+const GetAbilityInfo= async (url, abilityName)=>{
+    const data= await fetch(url);
+    const dataJson= await data.json();
+    const{effect_entries}= dataJson;
+
+    let effectsList='';
+
+    effect_entries.forEach(element => {
+
+        const{effect, language} = element;
+
+        if(language.name === 'en'){
+            //console.log(effect);
+            effectsList += `<li>${effect}</li>`;
+        }
+    });
+
+
+    pokemon.abilities.innerHTML += 
+    `<li>
+        ${abilityName}
+        <div>effect</div>
+        <ul>
+            ${effectsList}
+        </ul>
+    </li> `;
+
+  
+
+    //console.log(await dataJson);
+}
+
+GetPokemon();
+
